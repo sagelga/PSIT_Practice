7aae46ac89c03740e30c1083e2b1217bdf810c52
son9912 <son9912@gmail.com>
2016-11-20 15:33:45 +0700

    Do (1) file
    Changes:
    LEI_1826PointSorting.py

---
+++ LEI_1826PointSorting.py
@@ -0,0 +1,14 @@
+"""this is not finish answer. Do not reproduce."""
+def pointsort():
+    """this is a docstring"""
+    for _ in range(int(input())):
+        for _ in range(int(input())):
+            listing = []
+            data = input()
+            listing.append(data)
+            print(data)
+            data = data.split(" ")
+            data = [int(i) for i in data]
+            print(data)
+pointsort()
+
